b5800c0d3d3ff942416c87c0d43625c6e8b71645
Alex <agordon1123@gmail.com>
2019-09-04 12:52:25 -0400

    server listening properly

--- api/server.js
+++ api/server.js
@@ -5,3 +5,5 @@ const server = express();
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'up' })
 })
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('server');
+const server = require('./api/server');
 
 const port = 8000;
 server.listen(port, () => console.log(`API running on port ${port}`));
